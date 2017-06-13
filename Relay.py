4a6ae96fdc3dc243803cceb72567fe34c36c78f0
Gautam Krishnan Chittaranajana <GautamKrishnan@users.noreply.github.com>
2017-06-13 20:54:45 +0800

    a
    a

--- Relay.py
+++ Relay.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO
 import sys
 import time
 import datetime
+import pickle
 
 class Relay:
     def __init__(self):
@@ -17,7 +18,9 @@ class Relay:
         GPIO.output(pin, (GPIO.HIGH if state == 0 else GPIO.LOW))
 
     def getTimeOn(self, moistureValue):
-        if ( int(moistureValue) < 75 ):
+        if ( int(moistureValue == 0) ):
+            onTime = 0
+        elif ( int(moistureValue) > 45 and int(moistureValue) < 75):
             onTime = 5
         elif ( int(moistureValue) > 75 and int(moistureValue) < 150 ):
             onTime = 10
@@ -57,7 +60,7 @@ def main():
     print ' Switching on Sensor 1 '
     relay.switch(int(0), int(1))
     print ' Getting data from Sensor 1 '
-    time.sleep(30)
+    time.sleep(10)
     print "%s: moisture:%d" % (time.asctime(),
                                board.moisture())
     moistureValue1 = int(board.moisture())
@@ -67,11 +70,20 @@ def main():
     time.sleep(2)
 
     onTimeSen1 = relay.getTimeOn(moistureValue1);
+    dict1 = {'a':moistureValue1,'b':onTimeSen1}
+    try:
+        with open('sensor.pickle', 'wb') as f:
+            pickle.dump(dict1, f, protocol=pickle.HIGHEST_PROTOCOL)
+    except EOFError:
+        pass
+  
+       
+
 
     #MQTT Publisher
     client = Client(client_id="my_id_pub", userdata="user2")
     client.connect("172.17.249.19")
-    message = str("The senosor detected moisture level is %d and the corresponding time is %d" % (moistureValue1,onTimeSen1))
+    message = str("Moisture Level %d ,Time %d" % (moistureValue1,onTimeSen1))
     topic = "Moisture"
     client.publish(topic, message)
     
